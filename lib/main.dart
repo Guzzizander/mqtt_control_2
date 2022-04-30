695200ac98604169815fa0746655ad2b1b02c8c6
juanjo <jjpj@tinet.org>
2022-04-30 22:34:36 +0200

    Añadido slicer

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mqtt_client/mqtt_client.dart';
+import 'package:mqtt_client/mqtt_server_client.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() {
@@ -32,8 +33,54 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  String topic = 'prueba';
+  String broker = '192.168.0.133';
+  int port = 1883;
+  //String username = 'your_username';
+  //String password = 'your_password';
+  String clientIdentifier = 'GuzziZander';
+  String mensaje = 'DESCONECTADO';
+  bool estado = false;
+  MqttServerClient? client;
+  Color _iconColor = Colors.redAccent;
+  double _currentSliderValue = 0;
+  final builder = MqttClientPayloadBuilder();
+
+  void _publicaMensaje(String mensaje) {
+    builder.clear();
+    builder.addString(mensaje);
+    //client.publishMessage(topic, qosLevel, builder.payload);
+    client!.publishMessage(topic, MqttQos.atLeastOnce, builder.payload!);
+  }
+
+  Future<MqttServerClient> brokerSetup() async {
+    client = MqttServerClient.withPort(broker, clientIdentifier, port);
+    client!.logging(on: true);
+    client!.onConnected = onConnected;
+    client!.onDisconnected = onDisconnected;
+    client!.onSubscribed = onSubscribed;
+    client!.onSubscribeFail = onSubscribeFail;
+    client!.pongCallback = pong;
+    client!.secure = false;
+
+    final connMessage = MqttConnectMessage()
+        //.authenticateAs(username, password)
+        //.keepAliveFor(60)
+        .startClean()
+        .withWillQos(MqttQos.atLeastOnce)
+        .withClientIdentifier(clientIdentifier);
+    try {
+      await client!.connect();
+    } catch (e) {
+      setState(() {
+        mensaje = 'ERROR DE CONNEXION';
+      });
+      print('Exception: $e');
+    }
+    return client!;
+  }
+
   Widget build(BuildContext context) {
-    Color iconColor = Colors.red;
     return Scaffold(
       appBar: AppBar(
         title: const Text('MQTT Control 2'),
@@ -42,15 +89,13 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: <Widget>[
-            /* 
             // Contenedor donde se ven los mensajes del broker
             Container(
                 width: 200,
                 child: Text(
-                  messageFromBroker,
+                  mensaje,
                   textAlign: TextAlign.center,
                 )),
-                */
             SizedBox(
               height: 40,
             ),
@@ -58,10 +103,12 @@ class _MyHomePageState extends State<MyHomePage> {
               iconSize: 72,
               icon: const FaIcon(FontAwesomeIcons.circleUp),
               color: Colors.black,
-              onPressed: () {
-                print('Arriba');
-                //_publicaMensaje('Arriba');
-              },
+              onPressed: estado
+                  ? () {
+                      print('Arriba');
+                      _publicaMensaje('Arriba');
+                    }
+                  : null,
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
@@ -70,39 +117,36 @@ class _MyHomePageState extends State<MyHomePage> {
                   iconSize: 72,
                   icon: const FaIcon(FontAwesomeIcons.circleLeft),
                   color: Colors.black,
-                  onPressed: () {
-                    print('Izquierda');
-                  },
+                  onPressed: estado
+                      ? () {
+                          print('Izquierda');
+                          _publicaMensaje('Izquierda');
+                        }
+                      : null,
                 ),
+                // Boton de conexion/desconexion
                 IconButton(
                   iconSize: 72,
-                  icon: FaIcon((FontAwesomeIcons.circlePlay), color: iconColor),
+                  icon:
+                      FaIcon((FontAwesomeIcons.circlePlay), color: _iconColor),
                   onPressed: () {
-                    /*
-                    mqtt.Broker().brokerSetup();
-                    print('+++++++++++++++++++++++++++++++++++++++++++++++');
-                    String mensaje =
-                        mqtt.Broker().client!.connectionStatus.toString();
-                    print(mensaje);
-                    print('+++++++++++++++++++++++++++++++++++++++++++++++');
-                    setState(() {
-                      variables.iconColor = Colors.green;
-                      messageFromBroker = 'con';
-                      //variables.messageFromBroker = "Co";
-                      //variables.messageFromBroker =
-                      //cliente.connectionStatus.toString();
-                    });
-                    */
+                    if (!estado) {
+                      brokerSetup();
+                    } else {
+                      client!.disconnect();
+                    }
                   },
                 ),
                 IconButton(
                   iconSize: 72,
                   icon: const FaIcon(FontAwesomeIcons.circleRight),
                   color: Colors.black,
-                  onPressed: () {
-                    print('Derecha');
-                    //_publicaMensaje('Derecha');
-                  },
+                  onPressed: estado
+                      ? () {
+                          print('Derecha');
+                          _publicaMensaje('Derecha');
+                        }
+                      : null,
                 ),
               ],
             ),
@@ -110,14 +154,62 @@ class _MyHomePageState extends State<MyHomePage> {
               iconSize: 72,
               icon: const FaIcon(FontAwesomeIcons.circleDown),
               color: Colors.black,
-              onPressed: () {
-                print('Abajo');
-                //_publicaMensaje('Abajo');
-              },
+              onPressed: estado
+                  ? () {
+                      print('Abajo');
+                      _publicaMensaje('Abajo');
+                    }
+                  : null,
             ),
+            Slider(
+                value: _currentSliderValue,
+                max: 100,
+                divisions: 5,
+                label: _currentSliderValue.round().toString(),
+                onChanged: (double newValue) {
+                  setState() {
+                    _currentSliderValue = newValue;
+                    _publicaMensaje(_currentSliderValue.toString());
+                  }
+                }),
           ],
         ),
       ),
     );
+  }
+
+//actions
+  void onConnected() {
+    print('connected***********');
+    setState(() {
+      mensaje = 'CONECTADO';
+      estado = true;
+      _iconColor = Colors.greenAccent;
+    });
+  }
+
+  void onDisconnected() {
+    print('disconnected');
+    setState(() {
+      mensaje = 'DESCONECTADO';
+      estado = false;
+      _iconColor = Colors.redAccent;
+    });
+  }
+
+  void onSubscribed(String topic) {
+    print('subscribed to $topic');
+  }
+
+  void onSubscribeFail(String topic) {
+    print('failed to subscribe to $topic');
+  }
+
+  void on() {
+    print('disconnected');
+  }
+
+  void pong() {
+    print('ping response arrived');
   }
 }
